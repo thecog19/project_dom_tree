6b5a3fb11f552191d42d8cfd7a9cd5396688d8bb
Dexter Ford <dexter.ford.77@gmail.com>
2016-11-10 12:40:43 -0600

    Pseudocoded warmup

---
+++ lib/html_parser.rb
@@ -0,0 +1,11 @@
+class HTMLParser
+
+# takes in string
+# spits out object that represents tag & can be inquired of to get attributes (id, class, etc)
+  # get stuff inside brackets
+  # tag name = btwn < & " "
+  # attribute name is " " to = (strip)
+  # attribute content is = to " " (strip)
+  # store 'em all in Struct
+
+end
