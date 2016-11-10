32bea381cc4ecfe3a477268ff6d828c25315ef18
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-10 18:20:55 -0500

    Oh Oh. This is hard

--- lib/html_parser.rb
+++ lib/html_parser.rb
@@ -1,17 +1,26 @@
+require_relative "tag_generator"
+
 class HTMLParser
 
   def initialize(html_doc)
-    @page = File.open(html_doc)
+    @page = File.open(html_doc).readlines
+    @page = @page.join("")
   end
 
-  def parse_page
+  def parse_page(match_data = @page)
+    tag = scan_for_tag
+    grab_everything = /(?<=#{tag}\>)(\s*.*\s*)(?=\<\/#{tag}\>)/m 
+    grab_everything.match(match_data)
+    parse_page()
+    #add the text in, nodes for every parse down
+  end
 
+  def scan_for_tag
+    tag_scan_regex = /<(.*?)>/m
+    data = @page.match(tag_scan_regex)
+    data[0][1..-2]
   end
 
 end
 
-# str = "niblets yoyoyoyoy niblets"
-# regex = /niblets(.*)niblets/
-# our_match = str.match(regex)
-# p our_match.captures
-# p our_match[1]
+HTMLParser.new("html_easy.html").parse_page
