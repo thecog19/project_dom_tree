fc0207e08408ee62bc808a768926f6642bb52804
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-10 14:08:23 -0500

    started working on parser

--- lib/html_parser.rb
+++ lib/html_parser.rb
@@ -1,5 +1,20 @@
+
+Tag = Struct.new(:type, :name, :id, :classes, :src,:title )
 class HTMLParser
 
+  def initialize(str)
+    @html_tag = str
+  end
+
+  def create_struct
+    tag = Tag.new(
+      type_parse,parse_for(name),parse(id),parse_for(classes),
+      parse_for(src), parse_for(title))
+  end
+
+  def type_parse
+
+  end
 # takes in string
 # spits out object that represents tag & can be inquired of to get attributes (id, class, etc)
   # get stuff inside brackets
